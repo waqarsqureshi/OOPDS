1dcee51f93eaf09cc4cc55a065f3302e244b3df3
waqarsqureshi <xyz@xyz>
2017-10-12 22:49:54 +0500

    changes made

--- examples/lecture03/linkedlistsingle2.cpp
+++ examples/lecture03/linkedlistsingle2.cpp
@@ -1,27 +1,31 @@
-// A complete working C program to demonstrate all insertion methods
+// A complete working C++ program to demonstrate all insertion methods
 // on Linked List
 #include <stdio.h>
 #include <stdlib.h>
  
 // A linked list node
-struct Node
+typedef struct Node
 {
   int data;
   struct Node *next;
-};
+  Node (int d, Node *n){
+        data = d;
+        next = n;
+  }
+} Node ;
  
 /* Given a reference (pointer to pointer) to the head of a list and 
    an int, inserts a new node on the front of the list. */
 void push(struct Node** head_ref, int new_data)
 {
     /* 1. allocate node */
-    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
- 
-    /* 2. put in the data  */
-    new_node->data  = new_data;
+    //struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+    Node* new_node = new Node(new_data,(*head_ref));    
+        /* 2. put in the data  */
+    //new_node->data  = new_data;
  
     /* 3. Make next of new node as head */
-    new_node->next = (*head_ref);
+    //new_node->next = (*head_ref);
  
     /* 4. move the head to point to the new node */
     (*head_ref)    = new_node;
